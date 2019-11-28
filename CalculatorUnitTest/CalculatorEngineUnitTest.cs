b8c12999ec7ce36385cce1e0b0460ecbd913d889
Johann Paque <johannpaque@gmail.com>
2019-11-28 13:48:16 +0100

    42

---
+++ CalculatorUnitTest/CalculatorEngineUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorEngineUnitTest
+    {
+        /// <summary>
+        /// Function to Addition
+        /// </summary>
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Add(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    
+    
+    
+    
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            int a = 6;
+            int b = 3;
+            int expected = 3;
+
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Sub(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodMul()
+        {
+            int a = 2;
+            int b = 3;
+            int expected = 6;
+
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Mul(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethodDivByZero()
+        {
+            int a = 6;
+            int b = 0;
+            int expected = Int32.MinValue;
+
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Div(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodDiv()
+        {
+            int a = 6;
+            int b = 3;
+            int expected = 2;
+
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Div(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+  
+} 
---
+++ Console/CalculatorEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public class CalculatorEngine
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Div(int a, int b)
+        {
+            int res;
+
+            
+
+            if(b != 0)
+            {
+                res = a / b;
+            } 
+            else
+            {
+                res = Int32.MinValue;
+            }
+
+            return res;
+        }
+    }
+}
